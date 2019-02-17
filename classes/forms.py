5ffeec3384afdfc68409f4be9adb4e144511b345
FahadAlMutairi5 <almteref@gmail.com>
2019-02-17 20:37:18 +0300

    mini project classrooms v1

--- classes/forms.py
+++ classes/forms.py
@@ -1,7 +1,30 @@
 from django import forms
-from .models import Classroom
+from .models import Classroom, Student
+from django.contrib.auth.models import User
 
 class ClassroomForm(forms.ModelForm):
     class Meta:
         model = Classroom
-        fields = '__all__'
+        fields = ['name', 'subject', 'year']
+
+class SignupForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['username', 'first_name', 'last_name', 'email' ,'password']
+
+        widgets={
+        'password': forms.PasswordInput(),
+        }
+
+class StudentForms(forms.ModelForm):
+    class Meta:
+        model = Student
+        fields = ['name', 'gender', 'date_of_birth', 'exam_grade']
+        widgets = {
+            #NOT Use localization and set a default format
+            'date_of_birth': forms.DateTimeInput(attrs={'type':'date'}),
+        }
+        
+class SigninForm(forms.Form):
+    username = forms.CharField(required=True)
+    password = forms.CharField(required=True, widget=forms.PasswordInput())
--- classes/models.py
+++ classes/models.py
@@ -1,10 +1,34 @@
 from django.db import models
 from django.urls import reverse
+from django.contrib.auth.models import User
+
 
 class Classroom(models.Model):
 	name = models.CharField(max_length=120)
 	subject = models.CharField(max_length=120)
 	year = models.IntegerField()
+	teacher = models.ForeignKey(User, on_delete=models.CASCADE)
 
 	def get_absolute_url(self):
 		return reverse('classroom-detail', kwargs={'classroom_id':self.id})
+
+class Student(models.Model):
+	name = models.CharField(max_length=120)
+	date_of_birth = models.DateTimeField()
+
+	GENDER_CHOICES = (
+        ('GENDER', 'Gender'),
+        ('MALE', 'Male'),
+        ('FEMALE', 'Female'),
+    )
+	gender = models.CharField(
+        max_length=6,
+        choices=GENDER_CHOICES,
+        default='GENDER',
+    )
+	exam_grade = models.FloatField()
+	classroom = models.ForeignKey(Classroom, on_delete=models.CASCADE, related_name='students')
+
+	
+
+		
